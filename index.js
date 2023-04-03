3355fcaf6965cf37183f4b052dd1282ec305f075
Rodrigo <rromero22398@gmail.com>
2023-04-03 17:21:21 -0400

    arrow functions

---
+++ index.js
@@ -0,0 +1,14 @@
+function divide(){
+    let a = 2000;
+    let b = 100;
+    return a/b;
+}
+const square = (x) => {
+    x = 2;
+    return x * x;
+}
+const add = (a,b) => {
+    a = 3;
+    b = 4;
+    return a + b;
+}
